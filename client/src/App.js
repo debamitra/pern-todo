aeb491faaa3bc74128b5e8d05b6c9dcc8c46bfc1
debamitra mukherjee <debamitram@sigmoidanalytics.com>
2021-11-24 14:18:51 +0530

    first commit

---
+++ client/src/App.js
@@ -0,0 +1,19 @@
+
+import './App.css';
+import React , { Fragment } from 'react';
+
+import InputTodo from "./components/InputTodo";
+import ListToDos from "./components/ListTodo";
+import ListTodos from './components/ListTodo';
+
+function App() {
+  return <Fragment>
+    <div className="container">
+    <InputTodo />
+    <ListTodos />
+      </div>
+
+  </Fragment>
+}
+
+export default App;
